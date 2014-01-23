7072a4ce25e73d3cb67d41a943fb9bfdfbafdddd
vivianm <vivianmbaji@gmail.com>
2014-01-23 12:12:59 +0000

    using a variant of the it method to collapse the code and description into one line

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,17 +7,9 @@ subject { page }
   describe "Home page" do
     before { visit root_path }
 
-    it "should have the content 'Sample App'" do
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the base title" do
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-    end
-
-    it "should not have a custom page title" do
-      expect(page).not_to have_title('| Home')  
-  end
+    it { should have_content('Sample App') }
+    it { should have_title("Ruby on Rails Tutorial Sample App") }
+    it { should_not have_title('| Home') }
 end
 
     describe "Help page" do
