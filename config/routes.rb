8ddecfcdc5167dd0339ea301fa690dc1062f5adf
vivianm <vivianmbaji@gmail.com>
2014-01-23 10:35:12 +0000

    Establishing the route mapping for the Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  match '/', to: 'static_pages#home', via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
