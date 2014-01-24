e4f5a421f37c9ba784a0f4eed332412919d61e6d
vivianm <vivianmbaji@gmail.com>
2014-01-24 05:53:06 +0000

    A test for the links on the layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -38,4 +38,19 @@ end
     it { should have_selector('h1', text: 'Contact') }
     it { should have_title(full_title('Contact')) }
   end
+  
+    it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to # fill in
+    click_link "Contact"
+    expect(page).to # fill in
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to # fill in
+    click_link "sample app"
+    expect(page).to # fill in
+  end
 end
