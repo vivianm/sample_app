e82429a927cac2b00f2b90fed960990edbf648d7
vivianm <vivianmbaji@gmail.com>
2014-01-25 08:49:42 +0000

    complete exercise6

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -38,25 +38,4 @@ end
     it { should have_selector('h1', text: 'Contact') }
     it { should have_title(full_title('Contact')) }
   end
-  
-    it "should have the right links on the layout" do
-    visit root_path
-    click_link "About"
-    expect(page).to have_title(full_title('About Us'))
-    click_link "Help"
-    expect(page).to have_title(full_title('Help'))
-    click_link "Contact"
-    expect(page).to have_title(full_title('Contact')) 
-    click_link "Home"
-    click_link "Sign up now!"
-    expect(page).to have_title(full_title('Sign up now!')) 
-    click_link "sample app"
-    expect(page).to have_title(full_title('sample app')) 
-    expect(page).to have_title(full_title('Contact'))
-    click_link "Home"
-    click_link "Sign up now!"
-    expect(page)#.to have_title('| Home')
-    click_link "sample app"
-    expect(page)#.to have_title(full_title('sample app'))
-  end
 end
