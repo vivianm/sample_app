0f932d6bc86a4ecd37dbcddba78278ebd44e1bb8
vivianm <vivianmbaji@gmail.com>
2014-01-25 07:17:59 +0000

    Getting the initial password tests to pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
 
   	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
+
+     has_secure_password                
 end
