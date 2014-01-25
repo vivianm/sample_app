9cd2009c2b0a0172aacb4f6fbdc856da35ef168c
vivianm <vivianmbaji@gmail.com>
2014-01-25 10:07:15 +0000

    Adding a User model object so asto enable codes in user controller

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,3 +11,11 @@ describe "User pages" do
     it { should have_title(full_title('Sign up')) }
   end
 end
+
+describe "profile page" do
+  # Replace with code to make a user variable
+  before { visit user_path(user) }
+
+  it { should have_content(user.name) }
+  it { should have_title(user.name) }
+end
