9a707a950eb97351e006e526e0a3a2e49c0aa80d
vivianm <vivianmbaji@gmail.com>
2014-01-27 06:45:51 +0000

    The tests for signin failure

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,10 +4,14 @@ describe "Authentication" do
 
 	subject { page }
 
-    describe "signin page" do
+    describe "signin" do
       before { visit signin_path }
 
-      it { should have_content('Sign in') }
+      describe "with invalid information" do
+      before { click_button "Sign in" }
+
       it { should have_title('Sign in') }
+      it { should have_selector('div.alert.alert-error') }
   end
+ end
 end
