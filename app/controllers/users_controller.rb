a4a90643ed72fe9fa01a0bf9977b1a0f78766556
vivianm <vivianmbaji@gmail.com>
2014-01-27 13:49:37 +0000

    Adding store_location to the signed-in user before filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,8 +43,11 @@ class UsersController < ApplicationController
      # Before filters
 
     def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
     end
+  end
 
     def correct_user
       @user = User.find(params[:id])
