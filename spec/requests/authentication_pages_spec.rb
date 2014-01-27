3b21b10220fa6b462da2b57d87dac368863076d5
vivianm <vivianmbaji@gmail.com>
2014-01-27 07:13:57 +0000

    Correct tests for signin failure

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -12,6 +12,11 @@ describe "Authentication" do
 
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
   end
 
    describe "with valid information" do
@@ -26,6 +31,5 @@ describe "Authentication" do
       it { should have_link('Profile',     href: user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
-    end
   end
 end
