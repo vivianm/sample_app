57be5a2517946af9dd11ff416848583abf0c55f9
vivianm <vivianmbaji@gmail.com>
2014-01-27 15:19:41 +0000

    Tests for pagination

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,15 +5,21 @@ describe "User pages" do
 	subject { page }
 
   describe "index" do
-    before do
-      sign_in FactoryGirl.create(:user)
-      FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
-      FactoryGirl.create(:user, name: "Ben", email: "ben@example.com")
+    let(:user) { FactoryGirl.create(:user) }
+    before (:each) do
+      sign_in user
       visit users_path
     end
 
     it { should have_title('All users') }
     it { should have_content('All users') }
+
+    describe "pagination" do
+
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all)  { User.delete_all }
+
+    it { should have_selector('div.pagination') }
 
     it "should list each user" do
       User.all.each do |user|
@@ -21,6 +27,7 @@ describe "User pages" do
       end
     end
   end
+end
 
     describe "profile page" do
       let(:user) { FactoryGirl.create(:user) }
