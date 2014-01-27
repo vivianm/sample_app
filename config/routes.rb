1907da9db9d8f55328eae646cb94cab221a64187
vivianm <vivianmbaji@gmail.com>
2014-01-27 06:25:30 +0000

    Adding a resource to get the standard RESTful actions for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
