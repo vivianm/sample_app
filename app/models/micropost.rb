5c2cab7d1adb307a445ebe1f6d45fadf81c6fb6a
vivianm <vivianmbaji@gmail.com>
2014-01-29 13:59:10 +0000

    Improving from_users_followed_by

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,8 +4,10 @@ class Micropost < ActiveRecord::Base
 	 validates :content, presence: true, length: { maximum: 140 }
 	 validates :user_id, presence: true
 
+# Returns microposts from the users being followed by the given user.
 def self.from_users_followed_by(user)
     followed_user_ids = user.followed_user_ids
-    where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+    where("user_id IN (:followed_user_ids) OR user_id = :user_id", 
+    	   followed_user_ids: followed_user_ids, user_id: user)
   end
 end
